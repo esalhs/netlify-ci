2a1911d475da5b16c9895c0561aceffcd3e729a8
Elina Shogenova <elina.shog04@gmail.com>
2025-11-01 16:00:16 +0300

    trying bundle again

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const browserSync = require('browser-sync').create();
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mediaquery = require('postcss-combine-media-query');
+const cssnano = require('cssnano');
 
 function serve() {
   browserSync.init({
@@ -25,7 +26,8 @@ function html() {
 function css() {
   const plugins = [
     autoprefixer(),
-    mediaquery()
+    mediaquery(),
+    cssnano()
   ];
   return gulp.src('src/**/*.css')
         .pipe(plumber())
